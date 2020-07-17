9748a3953f72cbdb860bd54a457fa8d21c853650
Varun Raj <varunraj82@gmail.com>
2020-07-16 21:05:47 -0400

    added child component and props

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 import faker from 'faker'
 
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 5:45pm" 
-                blogText="This is good" 
-                imageSrc={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Alex" 
-                timeAgo="Today at 1:50pm" 
-                blogText="This is excellent" 
-                imageSrc={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="Today at 8:00pm" 
-                blogText="This is great" 
-                imageSrc={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 5:45pm" 
+                    blogText="This is good" 
+                    imageSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 1:50pm" 
+                    blogText="This is excellent" 
+                    imageSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Today at 8:00pm" 
+                    blogText="This is great" 
+                    imageSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     )
 }
